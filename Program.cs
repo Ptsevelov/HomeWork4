590e07431100e3d00b6fe418b4eff6be69381577
Pavel <p.tsevelov@gmail.com>
2023-01-21 14:38:18 +0700

    Выполнение ДЗ

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿
+// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B. 
+
+/*Console.WriteLine($" Возведение числа A в натуральную степень B");
+
+ Console.Write("Введите число A: ");
+  int numberA = Convert.ToInt32(Console.ReadLine());
+  Console.Write("Введите число B: ");
+  int numberB = Convert.ToInt32(Console.ReadLine());
+
+  int exponentiation = Exponentiation(numberA, numberB);
+  Console.WriteLine("Ответ: " + exponentiation);
+
+
+int Exponentiation(int numberA, int numberB)
+{
+  int result = 1;
+  for(int i=1; i <= numberB; i++)
+  {
+    result = result * numberA;
+  }
+    return result;
+}*/
+
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+/*Console.Write("Введите число: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+  int SumNumber(int numberN){
+    
+    int counter = Convert.ToString(numberN).Length;
+    int advance = 0;
+    int result = 0;
+
+    for (int i = 0; i < counter; i++){
+      advance = numberN - numberN % 10;
+      result = result + (numberN - advance);
+      numberN = numberN / 10;
+    }
+   return result;
+  }
+
+int sumNumber = SumNumber(numberN);
+Console.WriteLine("Сумма цифр в числе: " + sumNumber);*/
+
+//Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. (числа берете любые)
+
+int [] Array = GetArray(8);
+Console.Write($"[{String.Join(";", Array)}]");
+
+int[] GetArray (int size)
+{
+    int[] Array = new int[size];
+    for (int i=0; i < size; i++)
+    {
+        Array[i] = new Random().Next(36);
+    }
+    return Array;
+}
